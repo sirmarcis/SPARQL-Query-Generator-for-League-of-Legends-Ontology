d148a607c420ded57abc7cc824bc92e575d3a303
Anders Maraviglia <anders_Maraviglia@yahoo.com>
2017-04-27 14:51:22 -0400

    Cleaned up code and made arguments be taken in thru the command line"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import org.apache.jena.atlas.lib.Pair;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.rdf.model.impl.LiteralImpl;
 import org.apache.jena.util.FileManager;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -31,7 +36,11 @@ public class Main {
      */
 
 
-
+    /**
+     * Called by getNextItemQuery and getItemBuildsIntoQuery
+     * @param currentItems, the current items in the users inventory
+     * @return a formatted SPARQL query line containing all items
+     */
     private static String getRootItemsStr(ArrayList<String> currentItems){
         StringBuilder s = new StringBuilder();
         s.append("\tFILTER (");
@@ -47,6 +56,11 @@ public class Main {
         return s.toString();
     }
 
+    /**
+     * Called by getNextItemQuery
+     * @param currentItems, the current items in the users inventory
+     * @return a formatted SPARQL query line containing all items
+     */
     private static String getPrecludeItemsString(ArrayList<String> currentItems){
         StringBuilder s = new StringBuilder();
         s.append("\tFILTER (");
@@ -62,6 +76,13 @@ public class Main {
         return s.toString();
     }
 
+    /**
+     * Called by getItemSuggestions
+     * @param currentItems, array list of the users current items
+     * @param goldAvailable, the users current gold
+     * @param inventorySpace, the number of open slots in the users inventory
+     * @return a SPARQL query string to get all 'nextItemInBuild' relationships
+     */
     private static String getNextItemQuery(ArrayList<String> currentItems, int goldAvailable, int inventorySpace){
         StringBuilder s = new StringBuilder();
         String itemQueryPrefix = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
@@ -87,6 +108,13 @@ public class Main {
         return s.toString();
     }
 
+    /**
+     * Called by getItemSuggestions
+     * @param currentItems, array list of the users current items
+     * @param goldAvailable, the users current gold
+     * @param inventorySpace, the number of open slots in the users inventory
+     * @return a SPARQL query string to get all 'buildsInto' relationships
+     */
     private static String getItemBuildsIntoQuery(ArrayList<String> currentItems, int goldAvailable, int inventorySpace){
         StringBuilder s = new StringBuilder();
         String itemQueryPrefix = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
@@ -103,15 +131,19 @@ public class Main {
         String rootItemsStr = getRootItemsStr(currentItems);
         s.append(rootItemsStr);
         String precludeItemsStr = getPrecludeItemsString(currentItems);
-        //s.append(precludeItemsStr);
-        //s.append("\tFILTER(?nextitemcost <= ");
-        //s.append(goldAvailable);
-        //s.append(")\n");
         s.append("} group by ?item ?nextItem ?cost ?nextitemcost ?totalcost limit ");
         s.append(inventorySpace);
         return s.toString();
     }
 
+    /**
+     * Called by getItemSuggestions, runs a SPARQL query through the model
+     * @param itemQuery, the query string to be run
+     * @param model, the model object representing the ontology
+     * @param itemBuildsIntoRels, structure keeping track of which items build into which
+     * @param nextItemP, true when we are running a 'next item' query, false otherwise
+     * @return a mapping of affordable items to their gold costs
+     */
     private static HashMap<String, Integer> runItemQuery(String itemQuery, Model model, HashMap<String, String> itemBuildsIntoRels, boolean nextItemP){
         Query query = QueryFactory.create(itemQuery);
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
@@ -129,7 +161,6 @@ public class Main {
                         if (!itemBuildsIntoRels.containsKey(currItem)) {
                             itemBuildsIntoRels.put(currItem, nextItem);
                         }
-                        //System.out.println("item: " + currItem + " builds into " + nextItem + ", nextItemCost: " + nextItemCost);
                     }
                 }
             }
@@ -139,15 +170,45 @@ public class Main {
         return items;
     }
 
-    private static HashMap<String, Integer> getItemSuggestions(Model model, ArrayList<String> currentItems, HashMap<String, Integer> ownedItems, int goldAvailable, int inventorySpace){
+    /**
+     * Called by getItemSuggestions, writes a SPARQL query to file
+     * @param theQuery, the query string to write to file
+     * @param destFilename, the file name the query will be saved under
+     */
+    private static void writeQueryToFile(String theQuery, String destFilename){
+        String outputDirpath = System.getProperty("user.dir");
+        File outputF = new File(outputDirpath + "/generatedQueries");
+        if(!outputF.exists()){ // ensure directory for output queries exists
+            outputF.mkdir();
+        }
+        try {
+            PrintWriter writer = new PrintWriter(outputDirpath + "/generatedQueries/" + destFilename, "UTF-8");
+            writer.println(theQuery);
+            writer.close();
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    /**
+     * Called by main and itself (recursive)
+     * @param model, the model object representing the ontology
+     * @param currentItems, array list of the users current items
+     * @param ownedItems, a mapping of all items to their respective costs
+     * @param goldAvailable, the users current gold
+     * @param inventorySpace, the number of open slots in the users inventory
+     * @return A hash map of suggested items to buy and their respective costs
+     */
+    private static HashMap<String, Integer> getItemSuggestions(Model model, ArrayList<String> currentItems, HashMap<String, Integer> ownedItems, int goldAvailable, int inventorySpace, int depth){
         HashMap<String, String> itemBuildsIntoRels = new HashMap<>();
         HashMap<String, Integer> itemSuggestionList = new HashMap<>();
         int runningGoldTot = goldAvailable;
         String nextItemQuery = getNextItemQuery(currentItems, goldAvailable, inventorySpace);
         String itemBuildsIntoQuery = getItemBuildsIntoQuery(currentItems, goldAvailable, inventorySpace);
+        writeQueryToFile(nextItemQuery, "nextItemQuery" + depth + ".txt");
+        writeQueryToFile(itemBuildsIntoQuery, "buildsIntoQuery" + depth + ".txt");
         HashMap<String, Integer> nextItems = runItemQuery(nextItemQuery, model, itemBuildsIntoRels, false);
         HashMap<String, Integer> buildsIntoItems = runItemQuery(itemBuildsIntoQuery, model, itemBuildsIntoRels, true);
-
         for(Map.Entry<String, Integer> ent : buildsIntoItems.entrySet()){
             int valOwnedCompItems = 0;
             if(itemBuildsIntoRels.containsValue(ent.getKey())){ // if an item is incomplete
@@ -180,50 +241,117 @@ public class Main {
                 tempCurrItems.add(ent.getKey());
                 HashMap<String, Integer> tempOwnedItems = new HashMap<>(ownedItems);
                 tempOwnedItems.put(ent.getKey(), ent.getValue());
-                HashMap<String, Integer> tempItemSuggestions = getItemSuggestions(model, tempCurrItems, tempOwnedItems, tempGoldAvailable, inventorySpace);
+                HashMap<String, Integer> tempItemSuggestions = getItemSuggestions(model, tempCurrItems, tempOwnedItems, tempGoldAvailable, inventorySpace, depth+1);
                 for(Map.Entry<String, Integer> ent2 : tempItemSuggestions.entrySet()){
-                    if(!newItemSuggestions.containsKey(ent2.getKey())){
+                    if(!newItemSuggestions.containsKey(ent2.getKey()))
                         newItemSuggestions.put(ent2.getKey(), ent2.getValue()+ent.getValue());
-                    }
                 }
             }
             itemSuggestionList.putAll(newItemSuggestions);
         }
         return itemSuggestionList;
+    }
 
+    private static Pair<ArrayList<String>, Integer> readCommandLineArgs(String[] args){
+        int currGold = -1;
+        boolean itemsP = false;
+        ArrayList<String> currItems = new ArrayList<>();
+        for(int x = 0; x < args.length; x++){
+            if(itemsP)
+                currItems.add(args[x]);
+            if(args[x].equals("--gold") && x+1 < args.length)
+                currGold = Integer.parseInt(args[x+1]);
+            else if (args[x].equals("--items"))
+                itemsP = true;
+        }
+        if(currGold == -1)
+            System.out.println("error, invalid command line args");
+        return new Pair<>(currItems, currGold);
+    }
+
+    private static String getOntologyArg(String[] args){
+        String ontologyFile = "";
+        for(int x = 0; x < args.length; x++){
+            if(args[x].equals("--ontology") && x+1 < args.length)
+                ontologyFile = args[x+1];
+        }
+        return ontologyFile;
+    }
+
+    /**
+     * Called by main, get the gold costs of all current items from the ontology
+     * @param currentItems, array list of the users current items
+     * @param model, the model object representing the ontology
+     * @return a mapping of all items to their respective costs
+     */
+    private static HashMap<String, Integer> getOwnedItemsHash(ArrayList<String> currentItems, Model model){
+        HashMap<String, Integer> ownedItems = new HashMap<>();
+        StringBuilder s = new StringBuilder();
+        String itemQueryPrefix = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX lol: <http://tw.rpi.edu/web/Courses/Ontologies/2017/LeagueOfLegends/LeagueOfLegends/>\n" +
+                "PREFIX lol-ind: <http://tw.rpi.edu/web/Courses/Ontologies/2017/LeagueOfLegends/LeagueOfLegends-Ind/>\n" +
+                "SELECT ?item ?cost\n" +
+                "WHERE {?item a lol:Item. \n" +
+                "\t?item lol:hasGoldCost ?cost.";
+        s.append(itemQueryPrefix);
+        String rootItemsStr = getRootItemsStr(currentItems);
+        s.append(rootItemsStr);
+        s.append("} group by ?item ?cost limit 20");
+        String itemQuery = s.toString();
+        Query query = QueryFactory.create(itemQuery);
+        QueryExecution qexec = QueryExecutionFactory.create(query, model);
+        try {
+            ResultSet results = qexec.execSelect();
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                String currItem = soln.getResource("item").getLocalName();
+                int currItemCost = soln.getLiteral("cost").getInt();
+                if(!ownedItems.containsKey(currItem))
+                    ownedItems.put(currItem, currItemCost);
+            }
+        } finally {
+            qexec.close();
+        }
+        return ownedItems;
     }
 
     public static void main(String[] args) {
-        String modelFilepath = System.getProperty("user.dir") + "/OE_11_LeagueOfLegends-Ind_V2.ttl";
+
+        Pair<ArrayList<String>, Integer> cLineArgs = readCommandLineArgs(args);
+        String ontologyFile = getOntologyArg(args);
+        if(cLineArgs.cdr() == -1) // return failure if invalid command line args
+            return;
+        String modelFilepath = System.getProperty("user.dir") + "/" + ontologyFile; // "/OE_11_LeagueOfLegends-Ind_V2.ttl";
         System.out.println("Model Filepath = " + modelFilepath);
         FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
         Model model = FileManager.get().loadModel(modelFilepath, null, "TURTLE");
 
-        // Example auto generate query
-        int goldAvailable = 3600;
-        int inventorySpace = 20;
-        ArrayList<String> currentItems = new ArrayList<>();
-        currentItems.add("NoneItem");
-        currentItems.add("DoransBlade");
-        currentItems.add("WardingTotem");
+        int goldAvailable = cLineArgs.cdr(); // 3600;
+        int inventorySpace = 20; // currently static..
+        ArrayList<String> currentItems = cLineArgs.car(); //new ArrayList<>();
+        if(!currentItems.contains("NoneItem"))
+            currentItems.add("NoneItem");
+        //currentItems.add("DoransBlade");
+        //currentItems.add("WardingTotem");
         //currentItems.add("Pickaxe");
-        currentItems.add("HealthPotion");
-        currentItems.add("HealthPotion2");
-        HashMap<String, Integer> ownedItems = new HashMap<>();
-        ownedItems.put("NoneItem", 0);
-        ownedItems.put("DoransBlade", 450);
-        ownedItems.put("WardingTotem", 0);
+        //currentItems.add("HealthPotion");
+        //currentItems.add("HealthPotion2");
+        HashMap<String, Integer> ownedItems = getOwnedItemsHash(currentItems, model); //new HashMap<>();
+        //ownedItems.put("NoneItem", 0);
+        //ownedItems.put("DoransBlade", 450);
+        //ownedItems.put("WardingTotem", 0);
         //ownedItems.put("Pickaxe", 875);
-        ownedItems.put("HealthPotion", 50);
-        ownedItems.put("HealthPotion2", 50);
+        //ownedItems.put("HealthPotion", 50);
+        //ownedItems.put("HealthPotion2", 50);
 
-        HashMap<String, Integer> itemSuggestionList = getItemSuggestions(model, currentItems, ownedItems, goldAvailable, inventorySpace);
+        HashMap<String, Integer> itemSuggestionList = getItemSuggestions(model, currentItems, ownedItems, goldAvailable, inventorySpace, 0);
 
         for(Map.Entry<String, Integer> ent : itemSuggestionList.entrySet()){ // print results
             System.out.println("Can buy: " + ent.getKey() + " for " + ent.getValue() + " gold");
         }
-
-        //System.out.println(itemBuildsIntoQuery);
 
         /*
         String testQuery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
@@ -260,9 +388,7 @@ public class Main {
                     Literal currRoleLikelihoodLit = soln.getLiteral("roleLikelihood");
                     int currRoleLikelihood = Integer.parseInt(currRoleLikelihoodLit.getString());
                     System.out.println("\trole likelihood: " + currRoleLikelihood);
-
                 }
-
             }
         } finally {
             qexec.close();
